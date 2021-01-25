0773b7ab5d5ab13cb05dc1e501efaa38c17913a3
beymarjhoel <beymarjhlima@gmail.com>
2021-01-25 20:36:18 -0300

    Criando projeto Spring Boot no Iniatilizr

---
+++ excelsior/src/main/java/com/generation/excelsior/ExcelsiorApplication.java
@@ -0,0 +1,13 @@
+package com.generation.excelsior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelsiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelsiorApplication.class, args);
+	}
+
+}
---
+++ excelsior/src/test/java/com/generation/excelsior/ExcelsiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.excelsior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelsiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
